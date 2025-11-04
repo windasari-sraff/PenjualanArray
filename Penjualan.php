32ef8afc96a185140a17853097c83940cabd6539
Winda Sari <sariw3689@gmail.com>
2025-11-04 16:12:39 +0700

    commit 1,setup awal

---
+++ Penjualan.php
@@ -0,0 +1,6 @@
+<?php
+// Commit 1 – Setup awal
+$nama_barang = ["Laptop", "Printer", "Scanner", "Mouse", "Keyboard"];
+$harga_barang = [5000000, 3000000, 2000000, 150000, 250000];
+
+echo "--POLGAN MART--<br><br>";
